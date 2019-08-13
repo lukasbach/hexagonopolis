9855b3742b85fd5b940fcc627fb69e8cc7316a83
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-08-13 11:35:57 +0000

    Deploy lukasbach/hexagonopolis to github.com/lukasbach/hexagonopolis.git:gh-pages

---
+++ precache-manifest.d0d928cfd2817ca45987f955eeb81d8a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ceea3fea0dad0bde841d865fea9ebd1",
+    "url": "/hexagonopolis/index.html"
+  },
+  {
+    "revision": "4cd1f7614dcee60cc8f2",
+    "url": "/hexagonopolis/static/css/main.caa083ef.chunk.css"
+  },
+  {
+    "revision": "615da50adbe9a7504db4",
+    "url": "/hexagonopolis/static/js/2.bc45f361.chunk.js"
+  },
+  {
+    "revision": "4cd1f7614dcee60cc8f2",
+    "url": "/hexagonopolis/static/js/main.b7ec7417.chunk.js"
+  },
+  {
+    "revision": "6cab66f105454176d619",
+    "url": "/hexagonopolis/static/js/runtime~main.766f6878.js"
+  }
+]);
